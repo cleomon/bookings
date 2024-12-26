af3e325d2af5d64466389a8e7d825332cda8c744
Cleomon Junior <cleomonjunior@gmail.com>
2024-12-26 10:15:06 -0300

    Initial commit

---
+++ cmd/web/middleware.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"github.com/justinas/nosurf"
+	"net/http"
+)
+
+func WriteToConsole(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("Hit the page")
+		next.ServeHTTP(w, r)
+	})
+}
+
+// NoSurf is the csrf protection middleware
+func NoSurf(next http.Handler) http.Handler {
+	csrfHandler := nosurf.New(next)
+
+	csrfHandler.SetBaseCookie(http.Cookie{
+		HttpOnly: true,
+		Path:     "/",
+		Secure:   app.InProduction,
+		SameSite: http.SameSiteLaxMode,
+	})
+	return csrfHandler
+}
+
+// SessionLoad saves and load the session on every request
+func SessionLoad(next http.Handler) http.Handler{
+	return session.LoadAndSave(next)
+}
